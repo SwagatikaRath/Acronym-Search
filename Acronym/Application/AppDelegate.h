71e9b670f660d056b3d57763c13eda07b51bda08
Swagatika <reetu.swagatika@gmail.com>
2015-09-20 17:47:54 -0400

    //Initial Commit

--- Acronym/Application/AppDelegate.h
+++ Acronym/Application/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Acronym
 //
 //  Created by Swagatika on 9/19/15.
-//  Copyright (c) 2015 swagatika. All rights reserved.
+//  Copyright (c) 2015 swagatika.All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
